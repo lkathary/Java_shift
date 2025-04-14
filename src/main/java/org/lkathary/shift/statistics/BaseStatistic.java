f723565dbb52c66c1284e9a81ff5308a4484b661
Alexey <email@vipset.com>
2025-04-14 17:56:24 +0700

    Clean code

--- src/main/java/org/lkathary/shift/statistics/BaseStatistic.java
+++ src/main/java/org/lkathary/shift/statistics/BaseStatistic.java
@@ -1,12 +1,11 @@
 package org.lkathary.shift.statistics;
 
 import lombok.Getter;
-import org.lkathary.shift.models.CommandLine;
 import org.lkathary.shift.models.TypeStatistic;
 
 
 @Getter
-public abstract class BaseStatistic<T>  {
+public abstract class BaseStatistic<T> {
 
     protected Long count = 0L;
 
